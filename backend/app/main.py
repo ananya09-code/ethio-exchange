d4a5fbb6fc75bcb5e850d3bf54868cfd7164e40d
Ananya <ananyamangistu06@gmail.com>
2026-05-20 11:25:06 -0700

    small fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://ethio-exchange-yzzz.vercel.app"
+        "https://birrify.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
